51513172b698f8677c6670a06e1803a5d68b87b1
AnthonyG-Dev <giterugicheru@gmail.com>
2024-04-08 10:45:05 +0300

    Understand high order functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (number) => {
+  function fareMultiplier(fare) {
+    return fare * number;
+  }
+  return fareMultiplier;
+};
+
+const fareDoubler = (fare) => {
+  return fare * 2;
+};
+//solution
+// const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+  return fn(arrayOfDrivers);
+}
